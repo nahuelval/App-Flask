efa2600d6a6786e43ae51736f9c8f39c8097d869
nahuelval <nahuel04213@hotmail.com>
2022-12-27 21:07:47 -0300

    Carga de datos y creacion de rutas create y store

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flaskext.mysql import MySQL
 
 app = Flask(__name__)
@@ -16,12 +16,36 @@ def index():
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    sql = "insert into movie (nombre,descripcion,genero,actores) values('titanic','boat that skins','boats','a lot of people');"
+    sql = "SELECT * FROM movie;"
     cursor.execute(sql)
+
+    movies = cursor.fetchall()
+    print(movies)
 
     conn.commit()
 
-    return render_template('movies/index.html')
+    return render_template('movies/index.html', movies = movies)
+
+@app.route('/create')
+def create():
+    return render_template('movies/create.html')
+
+@app.route('/store', methods=['POST'])
+def store():
+    _nombre = request.form['txtNombre']
+    _descripcion = request.form['txtDesc']
+    _genero = request.form['txtGene']
+    _imagen = request.files['txtImg']
+
+    sql = "INSERT INTO movie (nombre,descripcion,genero,imagen) values(%s, %s, %s, %s);"
+    datos = (_nombre,_descripcion,_genero,_imagen.filename)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql, datos)
+    conn.commit()
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
