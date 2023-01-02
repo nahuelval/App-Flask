137e9669ed177fd56c2af4d4fa8bba75343437ed
nahuelval <nahuel04213@hotmail.com>
2023-01-02 14:11:51 -0300

    creacion de delete y modify

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 mysql = MySQL()
@@ -9,6 +10,9 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'movies'
+
+UPLOADS = os.path.join('uploads/')
+app.config['UPLOADS'] = UPLOADS
 
 mysql.init_app(app)
 
@@ -21,7 +25,6 @@ def index():
     cursor.execute(sql)
 
     movies = cursor.fetchall()
-    print(movies)
 
     conn.commit()
 
@@ -51,6 +54,73 @@ def store():
     conn = mysql.connect()
     cursor = conn.cursor()
     cursor.execute(sql, datos)
+    conn.commit()
+
+    return redirect('/')
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    sql = "DELETE FROM movie WHERE id=%s"
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql,id)
+    conn.commit()
+
+    return redirect('/')
+
+@app.route('/modify/<int:id>')
+def modify(id):
+    sql = "SELECT * FROM movie WHERE id=%s"
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql,id)
+    movie = cursor.fetchone()
+
+    conn.commit()
+
+    return render_template('movies/edit.html', movie=movie)
+
+@app.route('/update', methods=['POST'])
+def update():
+    _nombre = request.form['txtNombre']
+    _descripcion = request.form['txtDesc']
+    _genero = request.form['txtGene']
+    _imagen = request.files['txtImg']
+    id = request.form['txtId']
+
+    datos = (_nombre, _descripcion, _genero, id)
+
+    print(datos)
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    
+    sql = "SELECT image FROM movie WHERE id=%s"
+    cursor.execute(sql, id)
+
+    nombreFoto = cursor.fetchone()[0]
+
+    if _imagen.filename != '':
+        now = datetime.now()
+        tiempo = now.strftime("%Y%H%M%S")
+        nuevoNombreImagen = tiempo + '_' + _imagen.filename
+        _imagen.save('uploads/' + nuevoNombreImagen)
+        _imagen = nuevoNombreImagen
+
+        sql = "SELECT image FROM movie WHERE id=%s"
+        cursor.execute(sql, id)
+
+        os.remove(app.config['UPLOADS'] + nombreFoto)
+    else:
+        _imagen = nombreFoto
+
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    sql = f'UPDATE movie SET nombre="{_nombre}", descripcion="{_descripcion}", genero="{_genero}", image="{_imagen}" WHERE id={id}'
+    cursor.execute(sql)
     conn.commit()
 
     return redirect('/')
