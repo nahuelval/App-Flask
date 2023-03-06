7bed00237a7922747b588150b2e6dfbdc9af56ce
nahuelval <nahuel04213@hotmail.com>
2023-03-06 14:58:29 -0300

    404 template añadido

--- src/app.py
+++ src/app.py
@@ -78,6 +78,10 @@ def selectSearch(name, desc, gene):
 
 # RUTAS #
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html')
+
 @app.route('/moviepic/<path:nombreFoto>')
 def uploads(nombreFoto):
     return send_from_directory(os.path.join('uploads'), nombreFoto)
